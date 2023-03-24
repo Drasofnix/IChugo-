487047c1a9b5f792c345beb5393ba2793a6c7ac3
Drasofnix <86849234+Drasofnix@users.noreply.github.com>
2023-03-23 21:04:24 -0700

    cambios

--- api.js
+++ api.js
@@ -1,19 +1,9 @@
-import axios from "axios";
+const axios = require('axios');
 
-const URL="https://api.genshin.dev"
-export const getMainData = async (type) => {
-
-    console.log(type)
-    try {
-        const data = await axios.get(
-            `${URL}/${type}/all`
-        );
-        var datos = data.data;
-        console.log(datos);
-        return datos;
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-getMainData("characters");
+const fetchWeatherData = async () => {
+    const { data } = await axios.get(
+        'https://api.genshin.dev/characters/all'
+    );
+    console.log(data);
+}
+fetchWeatherData();
